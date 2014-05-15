38002b9d710d58d3623578c334d1b106d4ab402d
Donald Drummond <Donald.E.Drummond@gmail.com>
2014-05-15 14:38:07 -0400

    Wired collection into postsList template

--- client/views/posts/posts_list.js
+++ client/views/posts/posts_list.js
@@ -1,29 +1,5 @@
-var postsData = [
-  {
-    title: "introducing Telescope",
-    author: "Sacha Greif",
-    url: "http://sachagrief.com/introducing-telescope/"
-  },
-  {
-    title: "Meteor",
-    author: "Tom Coleman",
-    url: "http://meteor.com"
-  },
-  {
-    title: 'The Meteor Book',
-    author: 'Tom Coleman',
-    url: 'http://themeteorbook.com'
-  }
-];
-
 Template.postsList.helpers({
-  posts: postsData
-});
-
-Template.postItem.helpers({
-  domain: function() {
-    var a = document.createElement('a');
-    a.href = this.url;
-    return a.hostname;
+  posts: function() {
+    return Posts.find();
   }
 });
