2f9967b5b60795bdb969f3500056c5dea02e8d1d
Donald Drummond <Donald.E.Drummond@gmail.com>
2014-05-19 16:46:59 -0400

    Added post reordering animation.

--- client/views/posts/posts_list.js
+++ client/views/posts/posts_list.js
@@ -5,3 +5,13 @@ Template.postItem.helpers({
     return a.hostname;
   }
 });
+
+Template.postsList.helpers({
+  postsWithRank: function() {
+    this.posts.rewind();
+    return this.posts.map(function(post, index, cursor) {
+      post._rank = index;
+      return post;
+    });
+  }
+});
