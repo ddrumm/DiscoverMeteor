c637e6b7df769033506ef100a32b4480b6590e81
Donald Drummond <Donald.E.Drummond@gmail.com>
2014-05-19 00:23:46 -0400

    Monitor which errors have been seen, and clear on routing.

--- client/helpers/errors.js
+++ client/helpers/errors.js
@@ -1,5 +1,7 @@
 Errors = new Meteor.Collection(null);
-
 throwError = function(message) {
-  Errors.insert({message: message})
+  Errors.insert({message: message, seen: false})
+}
+clearError = function() {
+  Errors.remove({seen: true});
 }
