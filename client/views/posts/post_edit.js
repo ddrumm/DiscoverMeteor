1ac9a549518b09dfde36a7edc0dbcb2768ea4a03
Donald Drummond <Donald.E.Drummond@gmail.com>
2014-05-19 15:17:14 -0400

    Added routes for post lists, and pages to display them.

--- client/views/posts/post_edit.js
+++ client/views/posts/post_edit.js
@@ -20,11 +20,10 @@ Template.postEdit.events({
 
   'click .delete': function(e) {
     e.preventDefault();
-
     if (confirm("Delete this post?")) {
       var currentPostId = this._id;
       Posts.remove(currentPostId);
-      Router.go('postsList');
+      Router.go('home');
     }
   }
 });
